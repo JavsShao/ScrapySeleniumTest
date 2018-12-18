88fc1e9601d60928709cb61b4f8cd2abdf8aa851
hamdi <javs_shao@163.com>
2018-12-18 17:34:07 +0800

    实现start_requests

--- scrapyseleniumtest/scrapyseleniumtest/spiders/taobao.py
+++ scrapyseleniumtest/scrapyseleniumtest/spiders/taobao.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import quote
+
 import scrapy
+from scrapy import Request
 
 
 class TaobaoSpider(scrapy.Spider):
     name = 'taobao'
     allowed_domains = ['www.taobao.com']
-    start_urls = ['http://www.taobao.com/']
+    # start_urls = ['http://www.taobao.com/']
+    base_url = 'https://s.taobao.com/search?q='
+
+    def start_requests(self):
+        for keyword in self.settings.get('KWYWORDS'):
+            for page in range(1, self.settings.get('MAX_PAGE') + 1):
+                url = self.base_url + quote(keyword)
+                yield Request(url=url, callback=self.parse, meta={'page': page}, dont_filter=True)
 
     def parse(self, response):
         pass
